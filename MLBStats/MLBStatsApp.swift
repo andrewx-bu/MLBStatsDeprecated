325e7f66f7e0b4d7752efc72728210679c13d375
Andrew Xin <andrewx@bu.edu>
2024-07-14 16:21:41 -0700

    Initial Commit

---
+++ MLBStats/MLBStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLBStatsApp.swift
+//  MLBStats
+//
+//  Created by Andrew Xin on 7/14/24.
+//
+
+import SwiftUI
+
+@main
+struct MLBStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
